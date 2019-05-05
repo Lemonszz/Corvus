d1d3ed7cf2b020d23bf9d34e96b1d839577720bc
Sam <lemonverse@gmail.com>
2019-05-05 13:23:56 +1200

    Cleanup

--- src/main/java/party/lemons/corvus/item/ItemOrbOfImprisionment.java
+++ src/main/java/party/lemons/corvus/item/ItemOrbOfImprisionment.java
@@ -3,12 +3,9 @@ package party.lemons.corvus.item;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityList;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.boss.EntityWither;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemMonsterPlacer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -33,11 +30,7 @@ public class ItemOrbOfImprisionment extends ItemModel
 		{
 			putEntity(stack, target);
 			target.setDead();
-			System.out.println(stack.hasTagCompound() + " after");
-
 		}
-
-
 		return true;
 	}
 
@@ -46,11 +39,10 @@ public class ItemOrbOfImprisionment extends ItemModel
 	{
 		ItemStack stack = player.getHeldItem(hand);
 
-		System.out.println(hasEntity(player.getHeldItem(hand)) + " place" );
 		if(!worldIn.isRemote && hasEntity(stack))
 		{
-	//		EntityList.createEntityFromNBT(stack.getTagCompound().getCompoundTag("entity"), worldIn);
-		//	stack.setTagCompound(null);
+			EntityList.createEntityFromNBT(stack.getTagCompound().getCompoundTag("entity"), worldIn);
+			stack.setTagCompound(null);
 		}
 		return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
 	}
@@ -64,23 +56,19 @@ public class ItemOrbOfImprisionment extends ItemModel
 		if(tags == null)
 			tags = new NBTTagCompound();
 
+		//Write entity to tags
 		NBTTagCompound entityTags = new NBTTagCompound();
 		entity.writeToNBTAtomically(entityTags);
-
 		tags.setTag("entity", entityTags);
-		stack.setTagCompound(tags);
 
-		System.out.println(stack.hasTagCompound() + " input");
+		stack.setTagCompound(tags);
 	}
 
 	private boolean hasEntity(ItemStack stack)
 	{
-		System.out.println(stack.hasTagCompound());
-
-		if(!stack.hasTagCompound())
+		if(!stack.hasTagCompound())     //Always false??
 			return false;
 
-		System.out.println(stack.getTagCompound().hasKey("entity"));
 		return stack.getTagCompound().hasKey("entity");
 	}
 }
